152bd92084bbec9314f6dc08ef64b68517c2c026
Stuart Kuredjian <sgkur04@gmail.com>
2018-05-29 12:38:38 -0700

    add documentation

--- geru_challenge/geru_challenge/views/page_request.py
+++ geru_challenge/geru_challenge/views/page_request.py
@@ -1,3 +1,6 @@
+"""
+Module page_request - contains PageRequest class definition
+"""
 from sqlalchemy import Column, Integer, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
@@ -8,6 +11,10 @@ Base = declarative_base()
 
 
 class PageRequest(Base):
+    """
+    PageRequest class definition
+    :Base: declarative base that PageRequest is built on
+    """
     __tablename__ = 'requests'
     id = Column(Integer, primary_key=True)
     session_id = Column(String, nullable=False)
